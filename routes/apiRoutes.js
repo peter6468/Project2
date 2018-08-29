a066f73ebbe98bc6dacf313c040550789873a424
Torning, Fraser <Fraser_Torning@comcast.com>
2018-08-28 21:17:28 -0400

    fix port issue and add api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,34 @@
 var db = require("../models");
 
 module.exports = (app) => {
+  app.get("/api/survey", (req, res) => {
+    db.survey.findAll({}).then(data => {
+      res.json(data);
+    });
+  });
+
+  app.get("/api/survey/:surveyid", (req, res) => {
+    db.survey.findOne({ where: { id: req.params.surveyid } }).then(data => {
+      res.json(data);
+    });
+  });
+
+  app.get("/api/survey/:surveyid/axis", (req, res) => {
+    db.survey_axis.findAll({ where: { surveyid: req.params.surveyid } }).then(data => {
+      res.json(data);
+    });
+  });
+
+  
+
+  // "/api/survey/:surveyid/axis"
+  // "/api/survey/:surveyid/axis/:axisid"
+  // "/api/survey/:surveyid/axis/:axisid/question"
+  // "/api/survey/:surveyid/axis/:axisid/question/:questionid"
+  // "/api/survey/:surveyid/question"
+  // "/api/survey/:surveyid/question/:questionid"
+
+
   // Get all groups
   app.get("/api/group", (req, res) => {
     db.user_group.findAll({}).then(data => {
